d808eb924c9ac97656ca44207f567c5534a6c12c
Rohan Mehra <rohanmehra224466@gmail.com>
2024-12-14 10:46:39 +0530

    remove: left sidebar navigation

---
+++ src/components/Layout.jsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Prose } from '@/components/Prose'
+import { SectionProvider } from '@/components/SectionProvider'
+
+export function Layout({ children, sections = [] }) {
+  return (
+    <SectionProvider sections={sections}>
+      <div>
+        <motion.header
+          layoutScroll
+          className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
+        >
+          <div className="contents lg:pointer-events-auto lg:block">
+            <Header />
+          </div>
+        </motion.header>
+        <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+          <main className="py-16">
+            <Prose as="article">{children}</Prose>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </SectionProvider>
+  )
+}
