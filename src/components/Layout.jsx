f47f6ce70383553e286037430d00d08c786f0b69
Rohan Mehra <rohanmehra224466@gmail.com>
2024-12-20 22:04:11 +0530

    fix: 404 page, footer position

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,3 @@
-import { motion } from 'framer-motion'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Prose } from '@/components/Prose'
@@ -7,16 +6,18 @@ import { SectionProvider } from '@/components/SectionProvider'
 export function Layout({ children, sections = [] }) {
   return (
     <SectionProvider sections={sections}>
-      <div className="min-h-screen w-full">
+      <div className="flex min-h-screen flex-col">
         <Header />
-        <div className="relative mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center px-4 pt-20 sm:px-6 sm:pt-24 lg:px-8">
-          <main className="w-full py-8 sm:py-16">
-            <Prose as="article" className="mx-auto max-w-3xl px-4 sm:px-0">
-              {children}
-            </Prose>
-          </main>
-          <Footer className="mt-auto" />
+        <div className="flex-grow">
+          <div className="relative mx-auto flex max-w-7xl flex-col px-4 pt-20 sm:px-6 sm:pt-24 lg:px-8">
+            <main className="w-full py-8 sm:py-16">
+              <Prose as="article" className="mx-auto max-w-3xl px-4 sm:px-0">
+                {children}
+              </Prose>
+            </main>
+          </div>
         </div>
+        <Footer />
       </div>
     </SectionProvider>
   )
---
+++ src/pages/404.jsx
@@ -0,0 +1,21 @@
+import { Header } from '@/components/Header'
+import { HeroPattern } from '@/components/HeroPattern'
+
+export default function Custom404() {
+  return (
+    <div>
+      <HeroPattern />
+      <Header />
+      <div>
+        <div className="flex min-h-[70vh] flex-col items-center justify-center text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            404
+          </h1>
+          <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
+            Sorry, we could&apos;t find the page you&apos;re looking for.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
